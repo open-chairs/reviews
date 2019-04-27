6bb1a80db2c34fd57ea1a32d3e1f12f97bd4d65c
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-26 17:23:08 -0700

    re-organized files into src and dist folders in client

--- client/src/components/IndividualReviews.jsx
+++ client/src/components/IndividualReviews.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import fetch from 'node-fetch';
 
 import Post from './Post.jsx';
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Post extends React.Component {
 	constructor(props) {
