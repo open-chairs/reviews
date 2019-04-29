2a5e1780b685a7bf6f213babaa1e64686cdd0303
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-29 12:37:52 -0700

    remove foodtext post method functionality

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const app = express();
 const port = process.env.PORT || 3004;
 
 const db = require('./db');
-const foodProcessorAPI_KEY = require('./foodparser.config.js');
 
 const public = path.join(__dirname, '../client/dist');
 
@@ -24,21 +23,6 @@ app.get('/api/reviews/:id', (req, res) => {
 		else {res.send(results.rows)};
 	});
 });
-
-app.post('/api/foodtext', parser.text(), (req, res) => {
-	console.log('FOODTEXT:', req.body);
-	fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/detect', {
-		method: 'POST', 
-		headers: {
-			'X-RapidAPI-Key': foodProcessorAPI_KEY,
-			'ContentType': 'application/x-www-form-urlencoded'
-		},
-		body: `text=${req.body}`
-	})
-	.then(res => res.json())
-	.then(json => res.send(json))
-	.catch(err => res.send(404))
-})
 
 app.use('/(\\d+)/', express.static(public));
 
